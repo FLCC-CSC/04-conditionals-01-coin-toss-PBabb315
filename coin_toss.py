4907f17cc961c323aa2d760aa2deb07a77f9b74c
PBabb315 <patrickbabb777@gmail.com>
2026-03-01 17:51:51 +0000

    my solution to coin_toss.py

--- coin_toss.py
+++ coin_toss.py
@@ -1,23 +1,41 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME:Patrick Babb 
+# DATE:3/1/2026 
+# BRIEF DESCRIPTION: creating a coin toss program  
+
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-
-
-
-
+# Don't forget to import random!!!!!
+import random
+def main():
+    coin_toss()
+def coin_toss():
+    coin_flip = random.randint(0,100)
+    print('===== Coin Flipper =====')
+    if (coin_flip > 50):
+        print('Tails')
+    else:
+        print('Heads')
+main()
 
 
 
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -43,25 +61,12 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
+Putting in the definition and then assigning it to an if and else function was somewhat challenging as well as getting
+used to the syntax.
 
 
 
 
 
 
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
 '''
